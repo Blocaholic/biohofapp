c2d42ed0ca242a83ededdd694ea805059e3e83ac
Blocaholic <tobias@rocklands.de>
2023-07-31 06:45:37 +0200

    remove dark mode

--- www/app.mjs
+++ www/app.mjs
@@ -1,16 +1,2 @@
 const $ = id => document.getElementById(id);
 const $$ = query => document.querySelectorAll(query);
-
-const usesDarkMode = () => window.matchMedia('(prefers-color-scheme: dark)')?.matches || false;
-
-const switchMode = () => {
-  $('manifest').href = usesDarkMode() ? './dark.webmanifest' : './light.webmanifest';
-  $('icon').href = usesDarkMode() ? './icon-dark.svg' : 'icon.svg';
-};
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener(
-  "change",
-  switchMode
-);
-
-switchMode();
