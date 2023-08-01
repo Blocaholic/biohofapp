4db43d1e00d9580c1668db5d805390f92435be76
Blocaholic <tobias@rocklands.de>
2023-08-01 12:48:26 +0200

    add function to save username and password persistent in localStorage

--- www/app.mjs
+++ www/app.mjs
@@ -1,2 +1,14 @@
 const $ = id => document.getElementById(id);
 const $$ = query => document.querySelectorAll(query);
+
+const saveCredentialsPersistent = async (username, password) => {
+  localStorage.setItem('username', username);
+  localStorage.setItem('password', password);
+  if (navigator.storage && navigator.storage.persist) {
+    const alreadyPersisted = await navigator.storage.persisted();
+    const persistent = alreadyPersisted || (await navigator.storage.persist());
+    console.log(`persistent storage: ${persistent}`);
+    return persistent;
+  }
+  return false;
+};
