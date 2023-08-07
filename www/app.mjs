fd46acd637c25f6bab61df4249e404a205565e48
Blocaholic <tobias@rocklands.de>
2023-08-07 20:20:11 +0200

    add function 'randomString(length)'

--- www/app.mjs
+++ www/app.mjs
@@ -18,3 +18,21 @@ const fetchJson = async (url, options) => {
   const json = await response.json();
   return json;
 };
+
+const randomString = length => {
+  const chars =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const string = new Array(length)
+    .fill(0)
+    .map(_ => {
+      const uint32max = 2 ** 32;
+      const limit = uint32max - (uint32max % chars.length);
+      let r;
+      do {
+        r = crypto.getRandomValues(new Uint32Array(1))[0];
+      } while (r > limit);
+      return chars[r % chars.length];
+    })
+    .join('');
+  return string;
+};
