94f2dbe3d669900f77850c07c15c2be9a9820cdf
Blocaholic <tobias@rocklands.de>
2023-08-08 13:27:24 +0200

    Bugfix: Database::connect() did not work

--- php/Database.php
+++ php/Database.php
@@ -1,25 +1,26 @@
 <?php
 
 class Database {
-  private $host = 'localhost';
-  private $dbname = 'd03db2e1';
-  private $user = 'd03db2e1';
-  private $password = 'MEkkrvrT2KeLhcg2rM5B';
+  private static $dbhost = '127.0.0.1';
+  private static $dbname = 'd03db2e1';
+  private static $dbuser = 'd03db2e1';
+  private static $dbpassword = 'MEkkrvrT2KeLhcg2rM5B';
 
   public static function connect() {
     try {
-
-      $pdo = new PDO("mysql:dbname=$dbname;host=$host", $user, $password);
+      $pdo = new PDO(
+        "mysql:dbname=" . self::$dbname . ";host=" . self::$dbhost,
+        self::$dbuser,
+        self::$dbpassword
+      );
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       return $pdo;
     } catch (PDOException $e) {
-      throw new Exception (
-        'Verbindung zur Datenbank fehlgeschlagen!',
+      throw new Exception(
+        'Verbindung zur Datenbank fehlgeschlagen.',
         $e->getCode(),
         $e
       );
     }
   }
 }
-
-?>
