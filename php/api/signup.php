3a30f8f53ce5f45126ea96f39d83a2f3a40b7983
Blocaholic <tobias@rocklands.de>
2023-08-14 09:46:21 +0200

    first complete version of 'signup.php'

--- php/api/signup.php
+++ php/api/signup.php
@@ -1,58 +1,58 @@
 <?php
 
-require_once __DIR__ . '/../Database.php';
+require_once __DIR__ . '/../User.php';
+require_once __DIR__ . '/../Device.php';
 require_once __DIR__ . '/../Utils.php';
 
 header('Content-Type: application/json');
 
-if (empty($_POST["email"])) {
-  die('{"status": "error", "message": "Field \'email\' is required."}');
+$add_user_result = User::add($_POST['email']);
+$add_user_result_array = json_decode($add_user_result, true);
+
+if ($add_user_result_array['status'] !== 'success') {
+  die($add_user_result);
 }
 
-if (empty($_POST["password"])) {
-  die('{"status": "error", "message": "Field \'password\' is required."}');
+if (empty($add_user_result_array['userid'])) {
+  die('{"status": "error", "message": "Fehler beim Erstellen der \'userid\'."}');
 }
 
-if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-  die('{"status": "error", "message": "Valid email is required."}');
-}
-
-if (strlen($_POST["password"]) !== 32) {
-  die(
-    '{"status": "error", "message": "Password must be 32 characters."}'
-  );
-}
-
-$devicehash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+$userid = $add_user_result_array['userid'];
 $confirmationpassword = Utils::randomString(32);
-$confirmationhash = password_hash($confirmationpassword, PASSWORD_DEFAULT);
+$devicename = $_POST['devicename'];
+$devicepassword = $_POST['password'];
 
-try {
-  $pdo = Database::connect();
-  $query = "INSERT INTO unconfirmed_users (
-    email,
-    confirmationhash,
-    devicehash,
-    devicename
-  ) VALUES(
-    :email,
-    :confirmationhash,
-    :devicehash,
-    :devicename
-  );";
-  $data = ["email" => $_POST['email'], "confirmationhash" => $confirmationhash, "devicehash" => $devicehash, "devicename" => $_POST['devicename']];
-  $statement = $pdo->prepare($query);
-  $statement->execute($data);
-  // $insertedId = $pdo->lastInsertId();
-  $pdo = null;
-} catch (Exception $e) {
-  if (str_starts_with(
-    $e->getMessage(),
-    "SQLSTATE[23000]: Integrity constraint violation: 1062 Duplicate entry"
-  )) {
-    die('{"status": "error", "message": "E-Mail ist bereits vergeben."}');
-  }
-  die('{"status": "error", "message": "' . $e->getMessage() . '"}');
-};
+$add_device_result = Device::add(
+  $userid,
+  $devicename,
+  $devicepassword,
+  $confirmationpassword
+);
+$add_device_result_array = json_decode($add_device_result, true);
+$deviceid = $add_device_result_array['deviceid'];
 
-echo '{"status": "success"}';
+if (!send_confirmation_mail($_POST['email'], $deviceid, $devicename, $confirmationpassword)) {
+  die('{"status": "error", "message": "Bestätigungsmail konnte nicht versand werden."}');
+}
+
+echo '{"status": "success", "userid": "' . $userid . '", "deviceid": "' . $deviceid . '"}';
+
+function send_confirmation_mail(
+  $email,
+  $deviceid,
+  $devicename,
+  $confirmationpassword
+) {
+  $title = 'Bitte bestätige die Registrierung';
+  $content = '<h1>Ein Neues Gerät wurde registriert</h1>';
+  $content .= '<p>Geräte-ID: ' . $deviceid . '</p>';
+  $content .= '<p>Geräte-Name: ' . $devicename . '</p>';
+  $content .= '<p>E-Mail-Adresse: ' . $email . '</p>';
+  $content .= '<p><a href="https://biohofapp.de/confirm.php?confirmationpassword=' . $confirmationpassword . '">Bestätigen</a></p>';
+  $header_array[] = 'MIME-Version: 1.0';
+  $header_array[] = 'Content-type: text/html; charset=utf-8';
+  $header_array[] = 'From: BiohofApp.de <kontakt@biohofapp.de>';
+  $header = implode("\r\n", $header_array);
+  $mailSent = mail($email, $title, $content, $header);
+  return $mailSent;
+}
