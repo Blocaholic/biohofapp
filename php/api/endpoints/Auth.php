f8bb2357c4cb355a5c257f76483ca133cba2caf3
Blocaholic <tobias@rocklands.de>
2023-09-09 17:42:06 +0200

    'Auth::POST()': validate input

--- php/api/endpoints/Auth.php
+++ php/api/endpoints/Auth.php
@@ -1,10 +1,20 @@
 <?php
 
 class Auth {
-  public static function POST($deviceid) {
+  public static function POST($_deviceid) {
     require_once __DIR__ . '/../Token.php';
+    require_once __DIR__ . '/../Config.php';
 
-    $key = '123456789';
+    $_POST = json_decode(file_get_contents('php://input'), true);
+    $validated = self::validate_post_input($_POST, $_deviceid);
+
+    $deviceid = $validated['deviceid'];
+    $password = $validated['password'];
+
+    // get passhash from db
+    // validate passhash
+    // create payload
+
     $payload = json_encode([
       "iat" => time(),
       "email" => "test@mail.com",
@@ -12,12 +22,38 @@ class Auth {
       "deviceid" => "88",
     ]);
 
-    $token = Token::sign($payload, $key);
+    $token = Token::sign($payload, Config::token_key);
 
     http_response_code(201);
     return [
       "deviceid" => $deviceid,
       "token" => $token,
+    ];
+  }
+
+  private static function validate_post_input($input, $deviceid) {
+    require_once __DIR__ . '/../Utils.php';
+
+    Utils::equalsIntegerGreater0($deviceid) ?: http_response_exit(400, [
+      "message" => "'deviceid' must be an integer greater than 0",
+      "syntax" => "https://biohofapp.de/api/<endpoint>/<deviceid>",
+      "example" => "https://biohofapp.de/api/auth/123",
+      "deviceid" => $deviceid,
+    ]);
+
+    isset($input['password']) ?: http_response_exit(400, [
+      "message" => "Password is required.",
+    ]);
+
+    strlen($input['password']) === 32 ?: http_response_exit(400, [
+      "message" => "Password must be 32 characters.",
+      "password" => $input['password'],
+      "length" => strlen($input['password']),
+    ]);
+
+    return [
+      "deviceid" => (int) $deviceid,
+      "password" => $input['password'],
     ];
   }
 }
