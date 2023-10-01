f47c4176f7f08d8a235aa5586b19ab692b9b2aca
Blocaholic <tobias@rocklands.de>
2023-10-02 00:31:38 +0200

    handle confirmation links

---
+++ www/js/confirm.mjs
@@ -0,0 +1,33 @@
+import {$} from './$.mjs';
+import * as Error from './Error.mjs';
+import {fetchJson} from './Utils.mjs';
+
+const main = async () => {
+  const isNum = val => /^\d+$/.test(val);
+
+  const [id, confirmationpassword] = location.search.slice(1).split('&');
+
+  if (!id) return Error.show('Keine ID angegeben!');
+
+  if (!isNum(id)) return Error.show('ID muss eine Zahl sein!');
+
+  if (!confirmationpassword)
+    return Error.show('Kein Bestätigungspasswort angegeben!');
+
+  if (confirmationpassword.length !== 32)
+    return Error.show('Das Bestätigungspasswort muss 32 Zeichen lang sein!');
+
+  const result = await fetchJson(`./api/devices/${id}`, 'PATCH', {
+    operation: 'confirm',
+    confirmationpassword: confirmationpassword,
+  });
+
+  if (result.message) return Error.show(result.message);
+
+  if (result.httpResponseCode !== 200)
+    return Error.show(`Fehler: http_response_code: ${result.httpResponseCode}`);
+
+  $('success').style.display = '';
+};
+
+main();
