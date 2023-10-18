2c4e134bea69b8af4dc3f4c037d56968e326e41d
Blocaholic <tobias@rocklands.de>
2023-10-18 23:09:42 +0200

    "Farms::POST()": verify token

--- php/api/endpoints/Farms.php
+++ php/api/endpoints/Farms.php
@@ -2,8 +2,24 @@
 
 class Farms {
   public static function POST() {
+    require_once __DIR__ . '/../Token.php';
+    require_once __DIR__ . '/../Config.php';
+
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+    $token = $_POST['token'];
+    $KEY = Config::$token_key;
+
+    $payload = Token::verify($token, $KEY) ?: exit_with_error(401, [
+      "message" => "Invalid token.",
+    ]);
+
+    $userid = $payload['userid'];
+
+    // $farmid = add_farm_to_database() or exit_with_error();
 
     http_response_code(201);
-    return ['farmid' => 666];
+    return $userid;
+
   }
 }
