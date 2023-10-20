d6d730a9d25187cfcb6190c77f112e186dd59db6
Blocaholic <tobias@rocklands.de>
2023-10-20 10:43:29 +0200

    move token handling from "Farms::validate_post_input()" to "Farms::POST()"

--- php/api/endpoints/Farms.php
+++ php/api/endpoints/Farms.php
@@ -2,11 +2,25 @@
 
 class Farms {
   public static function POST() {
+    require_once __DIR__ . '/../Token.php';
 
     $_POST = json_decode(file_get_contents('php://input'), true);
-    $headers = apache_request_headers();
 
-    $farm = self::validate_post_input($_POST, $headers);
+    $token = apache_request_headers()['Token'] ?? exit_with_error(401, [
+      "message" => "Token is required.",
+    ]);
+
+    $token_payload = Token::verify($token) ?: exit_with_error(401, [
+      "message" => "Invalid token.",
+    ]);
+
+    $farm = self::validate_post_input($_POST);
+
+    if ($farm['owner'] !== $token_payload['userid']) {
+      exit_with_error(401, [
+        "message" => "Token does not belong to owner.",
+      ]);
+    }
 
     // $farmid = add_farm_to_database() or exit_with_error();
 
@@ -15,8 +29,7 @@ class Farms {
 
   }
 
-  private static function validate_post_input($input, $headers) {
-    require_once __DIR__ . '/../Token.php';
+  private static function validate_post_input($input) {
 
     $input['farmname'] ?? exit_with_error(400, [
       "message" => '"farmname" is required.',
@@ -41,20 +54,6 @@ class Farms {
     $input['module_bees'] ?? exit_with_error(400, [
       "message" => '"module_bees" is required.',
     ]);
-
-    $token = $headers['Token'] ?? exit_with_error(401, [
-      "message" => "Token is required.",
-    ]);
-
-    $token_payload = Token::verify($token) ?: exit_with_error(401, [
-      "message" => "Invalid token.",
-    ]);
-
-    if ($input['owner'] !== $token_payload['userid']) {
-      exit_with_error(401, [
-        "message" => "Token does not belong to owner.",
-      ]);
-    }
 
     if ($input['farmname'] < 3) {
       exit_with_error(400, [
