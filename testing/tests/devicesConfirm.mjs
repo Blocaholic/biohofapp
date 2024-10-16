bbb4ae14e994861e733fd8617741733c07c5ade0
Blocaholic <tobias@rocklands.de>
2024-10-16 17:10:18 +0200

    add tests for unknown operation, invalid id, missing password @ devices::confirm

--- testing/tests/devicesConfirm.mjs
+++ testing/tests/devicesConfirm.mjs
@@ -23,10 +23,66 @@ export const testDevicesConfirm = async function (user) {
     });
 
   console.log('#### unknown operation');
+  await fetch(`https://biohofapp.de/api/devices/${user.deviceid}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      operation: 'konfirm',
+      confirmationpassword: user.confirmationpassword,
+    }),
+  })
+    .then(response => {
+      it('http response code should be "400"', () =>
+        assert(response.status === 400));
+      return response.json();
+    })
+    .then(json => {
+      it('Error message should include "unknown operation"', () =>
+        assert.match(json.message.toLowerCase(), /unknown operation/));
+      it('Operation should be sent back', () =>
+        assert.match(json.operation, /konfirm/));
+    });
 
   console.log('#### invalid id');
+  await fetch(`https://biohofapp.de/api/devices/1b3`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      operation: 'confirm',
+      confirmationpassword: user.confirmationpassword,
+    }),
+  })
+    .then(response => {
+      it('http response code should be "400"', () =>
+        assert(response.status === 400));
+      return response.json();
+    })
+    .then(json => {
+      it('Error message should include "id must be an integer greater than 0"', () =>
+        assert.match(
+          json.message.toLowerCase(),
+          /id must be an integer greater than 0/
+        ));
+      it('ID should be sent back', () => assert.match(json.id, /1b3/));
+      it('json property "syntax" should exist', () => assert(json.syntax));
+      it('json property "example" should exist', () => assert(json.example));
+    });
 
   console.log('#### missing password');
+  await fetch(`https://biohofapp.de/api/devices/${user.deviceid}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      operation: 'confirm',
+      confirmationpasswort: user.confirmationpassword,
+    }),
+  })
+    .then(response => {
+      it('http response code should be "400"', () =>
+        assert(response.status === 400));
+      return response.json();
+    })
+    .then(json => {
+      it('Error message should include "password is required"', () =>
+        assert.match(json.message.toLowerCase(), /password is required/));
+    });
 
   console.log('#### password "= 32 chars');
 
